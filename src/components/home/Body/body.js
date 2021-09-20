9dc35715712f7293602cac8b169dfab26439b370
abhilashbairi01 <abhilashbairi9908@gmail.com>
2021-09-20 20:26:04 +0530

    deployed

--- src/components/home/Body/body.js
+++ src/components/home/Body/body.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-const-assign */
 import "./body.css";
 import Fbcomp from "../../home/social/fbcomp";
 import HAlfeatenApple from "../../../assets/images/contact_apple.svg";
 import Googleplay from "../../../assets/images/contact_google.svg";
 import { useState } from "react";
 import Heroimg from "../../../assets/images/hero.png";
+import React, { useRef } from "react";
 function Bodyfirst() {
   const [Info, setInfo] = useState({
     GetInTouchEmail: "",
   });
+
+  const ref = useRef();
+  const reset = () => {
+    ref.current.value = "";
+  };
   async function DirectToFirebase() {
     const res = await fetch(
       "https://infigondb-default-rtdb.firebaseio.com/InfigongetintouchBody.json",
@@ -26,22 +33,28 @@ function Bodyfirst() {
     // window.location.reload();
   }
   return (
+    
     <div>
       <div id="body">
         {/* <h1>fgdgdgd</h1> */}
         <img id="bodyimg" src={Heroimg} alt="Error" />
       </div>
       <div style={{ border: "0px solid red" }} id="Second_target">
-        <h1>Unlock Your Future With US.</h1>
-        <br />
-        <h2>
+        <div className="box">
+       <div className="textone">
+        <h1>UNLOCK your future with us.</h1>
+        </div>
+        <div className="texttwo"><h2>
           A one stop destination for your Career Selection, Education Planning
           and Skill development.
         </h2>
+        </div> 
         <br />
         <br />
         <br />
-        <br />
+        
+        </div>
+        
 
         <input
           style={{
@@ -49,18 +62,28 @@ function Bodyfirst() {
             height: "2.2rem",
             textAlign: "center",
             outline: "none",
+            border:"none",
+          
           }}
-          placeholder="Enter your Email"
+          placeholder="Enter your E-mail"
           type="text"
           onChange={(e) => {
                   setInfo({ ...Info, GetInTouchEmail: e.target.value });
+                  
                 }}
+            
+           ref={ref}      
         ></input>
         <span>
           <button
             onClick={(e) => {
+          
               e.preventDefault();
-              DirectToFirebase();
+              DirectToFirebase(); 
+              reset();              
+              
+              
+              
             }}
             id="buttonAll"
             style={{ outline: "none", border: "none" }}
@@ -74,17 +97,20 @@ function Bodyfirst() {
           className="temp"
           style={{ textAlign: "center", paddingRight: "3rem" }}
         >
-          <a
-            style={{ width: "10px" }}
-            href="https://infigon.page.link/2f6j4DqAp1Uqz7427"
+          <a href="https://infigon.page.link/2f6j4DqAp1Uqz7427" target="_blank">
+            <Fbcomp Icon={Googleplay} />
+          </a>
+          <a 
+          style={{ width: "10px" }}
+           href="https://infigon.page.link/2f6j4DqAp1Uqz7427"
             target="_blank"
           >
             <Fbcomp Icon={HAlfeatenApple} style={{ width: "1rem" }} />
           </a>
-          <a href="https://infigon.page.link/2f6j4DqAp1Uqz7427" target="_blank">
-            <Fbcomp Icon={Googleplay} />
-          </a>
+          
+          
         </div>
+       
       </div>
     </div>
   );
